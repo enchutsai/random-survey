6cf002b85e3920a5a1f27a315d23815a7e144489
enchutsai <quentin51241@gmail.com>
2025-03-31 10:31:15 +0800

    刪掉一些太多的 url.js

--- url.js
+++ url.js
@@ -6,16 +6,17 @@
  * 2. 包起來的網址之間用逗號分隔
  */
 
-
+/* 先關掉的
+ * https://docs.google.com/forms/d/e/1FAIpQLSdXRrBYnrooOLNzglxLIwUsXvCzdI-okOrqgsx3Mjzff7oq7w/viewform?usp=header
+ * https://docs.google.com/forms/d/e/1FAIpQLSeMog-VOZ6Ca-22C-M4UobuE8_ejSFLBMwsUyEzzEERrWB_iA/viewform?usp=header
+ * https://docs.google.com/forms/d/e/1FAIpQLSf-35mGOzRZVWHCTzYA4PBJ6-jMAYpt4KLuw5lq_bSQcY8tRQ/viewform?usp=header
+ * https://docs.google.com/forms/d/e/1FAIpQLSdDd_Tr9pGr3ToRXEzA5O3nszkgC1dvmvADemKCTlcVKgVdYg/viewform?usp=header
+ */
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSeMog-VOZ6Ca-22C-M4UobuE8_ejSFLBMwsUyEzzEERrWB_iA/viewform?usp=header',
 	'https://docs.google.com/forms/d/e/1FAIpQLSfLsYPfjKnoMhQDl7z06-nLND_SQk7yMIaj9kPFkhNnUeYedA/viewform?usp=header',
-	'https://docs.google.com/forms/d/e/1FAIpQLSf-35mGOzRZVWHCTzYA4PBJ6-jMAYpt4KLuw5lq_bSQcY8tRQ/viewform?usp=header',
 	'https://docs.google.com/forms/d/e/1FAIpQLSdftdZFaWY9nV9xK_6TTlIFX-M3fPEFMp2aMXKry1J1A765Fw/viewform?usp=header',
 	'https://docs.google.com/forms/d/e/1FAIpQLSeBuLUjOuHdV7t0qQqEr-Edt-NIQnYxRaec2qKivpdw-4dB9w/viewform?usp=header',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdXRrBYnrooOLNzglxLIwUsXvCzdI-okOrqgsx3Mjzff7oq7w/viewform?usp=header',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdtqpPLwW4tVDMK8D36cSkfp5z0iB6XhM8WKF_ArQ0oCR2v5A/viewform?usp=header',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdDd_Tr9pGr3ToRXEzA5O3nszkgC1dvmvADemKCTlcVKgVdYg/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSdtqpPLwW4tVDMK8D36cSkfp5z0iB6XhM8WKF_ArQ0oCR2v5A/viewform?usp=header'
 ];
 
 // 設定最低分配數量和總回應數
